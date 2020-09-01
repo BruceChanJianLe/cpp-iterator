771d439fcdd514d58bd3e6f22280b986793b6ee5
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-09-01 13:24:16 +0800

    add range nested loop

---
+++ range_nested_loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+int main(int argc, char ** argv)
+{
+    std::vector<double> ages {1.0, 1.1, 1.2, 1.3, 1.4, 1.5};
+
+    std::vector<std::vector<double>> ages_container {ages, ages, ages};
+
+    for(auto age_container : ages_container)
+    {
+        for(auto age : age_container)
+        {
+            std::cout << age << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
