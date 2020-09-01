c6e103e2aebf5e1eb77d93e82e5255702db593d7
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-09-01 13:23:59 +0800

    add range single

---
+++ range_single_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+
+int main(int argc, char ** argv)
+{
+    std::vector<double> ages {1.0, 1.1, 1.2, 1.3, 1.4, 1.5};
+
+    for(auto age : ages)
+    {
+        std::cout << age << "\t";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
